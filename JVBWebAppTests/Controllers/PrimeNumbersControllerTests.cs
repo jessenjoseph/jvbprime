2f5f66c8256994f2f86dce3754ff10e440ddcea5
jessenjoseph <34076986+jessenjoseph@users.noreply.github.com>
2022-11-18 09:29:12 +0000

    JVBWebAppTests

---
+++ JVBWebAppTests/Controllers/PrimeNumbersControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JVBWebApp.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JVBWebApp.Helpers;
+using Moq;
+
+namespace JVBWebApp.Controllers.Tests
+{
+    [TestClass()]
+    public class PrimeNumbersControllerTests
+    {
+        private readonly Mock<IPrimeNumbersGetter> _getter;
+        private readonly PrimeNumbersController _sut;
+        public PrimeNumbersControllerTests()
+        {
+            _getter = new Mock<IPrimeNumbersGetter>();
+            _sut = new PrimeNumbersController(_getter.Object);
+        }
+
+        [TestMethod()]
+        public void ShouldThrowExceptionIfGetterIsNull()
+        {
+            Assert.ThrowsException<ArgumentNullException>(() => new PrimeNumbersController(null));
+        }
+
+        [TestMethod()]
+        public void GetPrimeNumbersBeforeGivenTest()
+        {
+            var lists = _sut.GetPrimeNumbersBeforeGiven(5);
+
+            _getter.Verify(m => m.GetPrimeNumbersBeforeGivenNumber(5), Times.Once);
+        }
+    }
+}
---
+++ JVBWebAppTests/Helpers/PrimeNumbersGetterTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JVBWebApp.Helpers.Tests
+{
+    [TestClass()]
+    public class PrimeNumbersGetterTests
+    {
+        [TestMethod()]
+        public void GetPrimeNumbersBefore5()
+        {
+            var sut = new PrimeNumbersGetter();
+
+            var primeNumbersBefore5 = sut.GetPrimeNumbersBeforeGivenNumber(5);
+            Assert.IsNotNull(primeNumbersBefore5);
+
+            var expectedCollection = new List<int> { 1, 2, 3 };
+            Assert.AreEqual(expectedCollection.Count, primeNumbersBefore5.Count());
+            foreach (var expectedItem in expectedCollection)
+            {
+                Assert.IsTrue(primeNumbersBefore5.Any(t => t == expectedItem));
+            }
+        }
+
+        [TestMethod()]
+        public void GetPrimeNumbersBefore14()
+        {
+            var sut = new PrimeNumbersGetter();
+
+            var primeNumbersBefore14 = sut.GetPrimeNumbersBeforeGivenNumber(14);
+            Assert.IsNotNull(primeNumbersBefore14);
+
+            var expectedCollection = new List<int> { 1, 2, 3, 5, 7, 11, 13 };
+            Assert.AreEqual(expectedCollection.Count, primeNumbersBefore14.Count());
+            foreach (var expectedItem in expectedCollection)
+            {
+                Assert.IsTrue(primeNumbersBefore14.Any(t => t == expectedItem));
+            }
+        }
+    }
+}
